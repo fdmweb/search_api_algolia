629e8be83dbe1386a30efeaf25ca9e47975dfeec
abdirisaq <abd@fdm.dk>
2019-03-19 15:34:32 +0100

    Ensure the right Drupal version is installed.

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -1,4 +1,15 @@
-# Initialize composer_manager.
-php modules/composer_manager/scripts/init.php
-composer drupal-rebuild
+#!/bin/bash
+
+set -e $DRUPAL_TI_DEBUG
+
+# Ensure the right Drupal version is installed.
+# The first time this is run, it will install Drupal.
+# Note: This function is re-entrant.
+drupal_ti_ensure_drupal
+
+# Change to the Drupal directory
+cd "$DRUPAL_TI_DRUPAL_DIR"
+composer clearcache
+# Algolia Search
+composer global require "algolia/algoliasearch-client-php:^2.0"
 composer update -n --lock --verbose
