8996b555c90737c2bd26c95375bcbbc8a823ce33
abdirisaq <abd@fdm.dk>
2019-03-19 13:10:08 +0100

    Add composer require algolia/algoliasearch-client-php.

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -1,4 +1,7 @@
 # Initialize composer_manager.
 php modules/composer_manager/scripts/init.php
 composer drupal-rebuild
+# Change to the Drupal directory
+cd "$DRUPAL_TI_DRUPAL_DIR"
+composer require algolia/algoliasearch-client-php:^2.0
 composer update -n --lock --verbose
