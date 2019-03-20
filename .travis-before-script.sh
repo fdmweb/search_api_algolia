e48d419407cb495fd121a5dac7f8aa09702bd710
abdirisaq <abd@fdm.dk>
2019-03-20 11:16:44 +0100

    Add initialize composer manager.

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -9,23 +9,14 @@ drupal_ti_ensure_drupal
 
 # Change to the Drupal directory
 cd "$DRUPAL_TI_DRUPAL_DIR"
-echo "Show current DIR"
-pwd
 # Algolia Search
 composer require "algolia/algoliasearch-client-php:^2.0"
-cat composer.json | grep 'algolia'
-ls -la
-cd "vendor"
-ls -la
-
 cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
 
 # Manually clone the dependencies
 git clone --depth 1 --branch 8.x-1.x http://git.drupal.org/project/composer_manager.git
 
 # Initialize composer manager.
-TMP_CWD=`pwd`
-cd "$TMP_CWD"
 php "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH/composer_manager/scripts/init.php"
 composer drupal-rebuild
 composer update -n --verbose
